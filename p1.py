476a71703fdf3e8de970448661073575af0e757f
shruti-edelta <shruti.mungra@edeltaes.com>
2025-03-03 19:17:15 +0530

    second

--- p1.py
+++ p1.py
@@ -1,15 +1,16 @@
 # num_to_rom={1:"I",5:"V",10:"X",50:"L",100:"C",500:"D",1000:"M"}
 
 num_to_rom={1:'I',2:'II',3:'III',4:'IV',5:'V',10:'X'}
-
 n=int(input("enter number: "))
-
-if n>=20:
+r=str()
+if n<0 and n>=20:
     print("please enter num between 1 to 20")
 else:
-    print("yes")
-    for i in range(len(n)):
-        rem=n%10
+    if n in num_to_rom:
+        r+=num_to_rom[n]
+
+print("roman number : ",r)
+        
 
     
 
